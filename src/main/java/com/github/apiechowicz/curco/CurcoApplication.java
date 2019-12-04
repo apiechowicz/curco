918c816f8903857a8456fb9ed51c176bc3996742
Andrzej Piechowicz <15264663+apiechowicz@users.noreply.github.com>
2019-12-04 13:24:18 +0100

    Create project template

---
+++ src/main/java/com/github/apiechowicz/curco/CurcoApplication.java
@@ -0,0 +1,12 @@
+package com.github.apiechowicz.curco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurcoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/apiechowicz/curco/CurcoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.apiechowicz.curco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurcoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
